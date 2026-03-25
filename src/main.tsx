2b1c99f9e98c0c4e4c758498e66102156416a149
Zeteox <zeteox01@gmail.com>
2026-03-25 11:38:59 +0100

    feat: made the graph

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './globals.css'
+import { Graph } from './components/Graph'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Graph/>
   </StrictMode>,
 )
