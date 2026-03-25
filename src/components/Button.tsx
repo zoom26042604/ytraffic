0bca189d7162083ae6efd7d40ae53ca8b5c9e817
zoom26042604 <nathan.ferre@ynov.com>
2026-03-25 15:32:14 +0100

    fix: fix responsive

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -29,34 +29,35 @@ const Button = () => {
 
   return (
     <div className="flex justify-center items-center py-8">
-      <div className="grid grid-cols-2 gap-8 sm:grid-cols-3 items-end">
+      <div className="grid grid-cols-2 gap-3 sm:grid-cols-3 lg:gap-15 items-end">
         {dropdowns.map((dropdown, i) => (
           <div
             key={dropdown.label}
             className={`flex flex-col gap-2 relative ${i === 2 ? 'col-span-2 items-center sm:col-span-1' : ''}`}
           >
-            <label className="text-white text-lg font-medium text-center">{dropdown.label}</label>
+            <label className="text-white text-lg lg:text-xl xl:text-2xl font-medium text-center">{dropdown.label}</label>
             <button
               onClick={() => toggle(i)}
               className={`
-                px-21 py-2 border-2 rounded-4xl
+                px-18 py-4 border-2 rounded-4xl
                 sm:px-25 sm:py-2 sm:border-2 sm:rounded-4xl
                 md:px-26 md:py-3 md:border-2 md:rounded-4xl
-                lg:px-38 lg:py-6 lg:border-2 lg:rounded-4xl
+                lg:px-36 lg:py-6 lg:border-2 lg:rounded-4xl
+                xl:px-48 xl:py-8 xl:border-2 xl:rounded-4xl
                 bg-[#272727]
                 border-[#9C95DC] text-[#9C95DC]
-                hover:bg-[#9C95DC]/10 transition-colors
                 flex items-center justify-center relative
               `}
             >
               <span className="absolute left-5 text-sm">{selected[i] ?? ''}</span>
               <Triangle
-                className={`fill-[#9C95DC] absolute right-5 transition-transform duration-200 ${openIndex === i ? 'rotate-180' : 'rotate-270'}`}
+                size={15}
+                className={`fill-[#9C95DC] absolute right-3 sm:right-4 lg:right-5 transition-transform duration-200 ${openIndex === i ? 'rotate-180' : 'rotate-270'}`}
               />
             </button>
 
             {openIndex === i && (
-              <ul className="absolute top-full mt-2 w-full bg-[#2A2A2A] border border-[#9C95DC] rounded-xl overflow-hidden z-10">
+              <ul className="absolute top-full mt-2 w-full bg-[#272727] border border-[#9C95DC] rounded-xl overflow-hidden z-10">
                   <li
                   onClick={() => select(i, null)}
                   className="px-4 py-2 text-[#9C95DC]/40 hover:bg-[#9C95DC]/20 cursor-pointer text-sm italic"
--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -2,10 +2,10 @@ export function Graph() {
     return (
         <div className="flex flex-1 justify-center items-center py-10">
             <div className="relative border-purple-300 bg-[#272727] border-4 rounded-4xl
-                w-80 h-150
-                sm:w-150 sm:h-120
-                md:w-180 md:h-150
-                lg:w-250 lg:h-150">
+                w-80 h-80
+                sm:w-180 sm:h-120
+                md:w-200 md:h-150
+                lg:w-350 lg:h-150">
 
                 <div className="absolute -top-5 left-1/2 -translate-x-1/2
                     flex gap-5 sm:gap-15 md:gap-25 lg:gap-35 w-max">
